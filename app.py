5658fb9de56a20aa43b1b7f2b9c80c2c2e0e00c1
egorov.andrey <egorov.andrey@list.ru>
2021-04-10 16:52:19 +0300

    Поменяли параметры запуска

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, filename='app.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
-@app.route('/post', methods=['POST'])
+@app.route('/alice', methods=['POST'])
 def main():
 
     logging.info('Request: %r', request.json)
